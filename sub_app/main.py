7779377c193644ef0a3faff4a3346ca89d650f38
dmytro-dexter <dmytroboiko007@gmail.com>
2024-02-21 12:33:53 +0200

    add sub_app project

---
+++ sub_app/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI, Depends
+
+from .dependencies import get_query_token, get_token_header
+
+from .routers import items, users
+
+app = FastAPI(dependencies=[Depends(get_query_token)])
+
+app.include_router(items.router)
+app.include_router(users.router)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello Bigger Application"}
